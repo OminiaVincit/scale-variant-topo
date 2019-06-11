1adb2eeabe5a3aafc70064ce7a0cdfc7461f76f3
OminiaVincit <k09tranhoan@gmail.com>
2019-06-11 15:19:33 +0900

    Add modularity and mean first passage time features

--- make_common_features.py
+++ make_common_features.py
@@ -19,9 +19,13 @@ import os
 import argparse
 import re
 import scipy.io as sio
-import community as cm
+import community as cm #for modularity calculation
+
+import pykov #for expectation of global mean first passage time
+# https://github.com/riccardoscalco/Pykov
+
 import networkx as nx
-from multiprocessing import Process
+from multiprocessing import Process, Array
 import tempfile
 import time
 
@@ -53,12 +57,13 @@ def read_setting(filename):
     Reading setting file by each type of network
     """
     namelist = []
-    with open(filename, 'r') as rf:
+    with open(filename, 'r', encoding='utf-8') as rf:
         lines = rf.readlines()
         for line in lines:
             line = line.strip()
             name = line.replace('graphml', 'mat')
-            name = name.replace('.mat', '_adj.mat')
+            if '_adj.mat' not in name:
+                name = name.replace('.mat', '_adj.mat')
             namelist.append(name)
         rf.close()
     print('Number of files = {}'.format(len(namelist)))
@@ -82,10 +87,37 @@ def sum_dict(dic):
 def get_modularity(G):
     """
     Compute the maximum modularity using the Louvain heuristices
+    obtain different results at each run
     """
     part = cm.best_partition(G)
     md = cm.modularity(part, G)
     return md
+
+def get_EGMFPT(G):
+    """
+    Get expectation of global mean first passage time
+    """
+    A = nx.to_scipy_sparse_matrix(G)
+    A = A.todense()
+    H, L = A.shape[0], A.shape[1]
+    #P = np.zeros((H, L), dtype=np.float32)
+    dtrans = {}
+    for i in range(H):
+        ki = np.sum(A[i, :])
+        for j in range(L):
+            if ki == 0 or i == j:
+                dtrans[(i, j)] = 0
+            else:
+                dtrans[(i, j)] = A[i, j] / ki
+    T = pykov.Chain(dtrans)
+    mfpts = [np.mean([v for a, v in T.mfpt_to(i).items()]) for i in range(H)]
+    egm = np.average(mfpts)
+    if np.isnan(egm) or egm < 0 or egm >= 1e+6:
+        print('Mean of global mean first passage time not defined')
+        egm = 0.0
+    # for i in range(H):
+    #     print(i, T.mfpt_to(i))
+    return egm
 
 def network_statistics(G):
     # Average of local features
@@ -125,23 +157,31 @@ def network_statistics(G):
     global_efficiency = nx.global_efficiency(G)
     nums_connected = nx.number_connected_components(G)
     md = get_modularity(G)
-
-    features = [ass_coef, avg_cc, md, avg_deg_centrality, avg_shortest, avg_eccentricity, avg_node_closeness_centrality, 
+    egm = get_EGMFPT(G)
+    #egm = 0.0
+    features = [egm, md, ass_coef, avg_cc, avg_deg_centrality, avg_shortest, avg_eccentricity, avg_node_closeness_centrality, 
                 avg_node_betweeness_centrality, avg_edge_betweeness_centrality, avg_local_efficiency, avg_triangles,
                 density, transitivity, diameter, radius, global_efficiency, nums_connected]
     return features 
 
 
-def save_features_process(tmpfolder, filels, process_id):
+def save_features_process(filels, a, shape, bg_idx, process_id):
     print('Enter process {}'.format(process_id))
+    a = np.frombuffer(a.get_obj(), dtype=np.float32)
+    a.shape = shape
     features = []
+    count = 1
     for netfile in filels:
         G = read_network(netfile)
         features.append(network_statistics(G))
-    arr = np.asarray(features, dtype=np.float32)
-    tmpfile = '{}/features_{}'.format(tmpfolder, process_id)
-    np.save(tmpfile, arr)
-    print('Saved temporarily {}'.format(tmpfile))
+        if count % 10 == 0:
+            print('Process {} has finished to process {} / {} files'.format(process_id, count, len(filels)))
+        count += 1
+    a[bg_idx:(bg_idx + len(filels))] = np.asarray(features, dtype=np.float32)
+    print('Finished process {} begin at {} with {} files'.format(process_id, bg_idx, len(filels)))
+    # tmpfile = '{}/features_{}'.format(tmpfolder, process_id)
+    # np.save(tmpfile, arr)
+    # print('Saved temporarily {}'.format(tmpfile))
 
 
 def make_features(datname, infolder, outfolder, n_jobs=-1):
@@ -149,12 +189,21 @@ def make_features(datname, infolder, outfolder, n_jobs=-1):
     graphlist = os.path.join(infolder, '{}/graphlist.txt'.format(datname))
 
     if not os.path.isfile(graphlist):
-        print('File not found: {}'.format(graphlist))
+        # if not os.path.isfile(graphlist):
+        #     graphlist = os.path.join(infolder, '{}/matlist_grakel.txt'.format(datname))
+        if not os.path.isfile(graphlist):
+            print('File not found: {}'.format(graphlist))
 
     namels = read_setting(graphlist)
     N = len(namels)
     features = []
-    filels = ['{}/{}/{}'.format(infolder, datname, name) for name in namels]
+    if N > 0 and os.path.isfile(namels[0]):
+        filels = namels
+    else:
+        filels = ['{}/{}/{}'.format(infolder, datname, name) for name in namels]
+
+    # 18 features for each network
+    arr = np.zeros((N, 18), dtype=np.float32)
 
     if n_jobs <= 0:
         for i in range(N):
@@ -162,38 +211,50 @@ def make_features(datname, infolder, outfolder, n_jobs=-1):
             G = read_network(netfile)
             features.append(network_statistics(G))
             print('Processed {} files = {}'.format(i+1, netfile))
+        arr[...] = np.asarray(features)
+
     else:
         # Multiprocessing version
-        split_ls = [list(b) for b in np.array_split(filels, n_jobs)]
+        split_ls = [list(b) for b in np.array_split(range(N), n_jobs)]
+        split_ls = [[filels[i] for i in b] for b in split_ls]
+        shape = arr.shape
+        size = arr.size
+        arr.shape = size
+        marr = Array('f', arr)
+        #with tempfile.TemporaryDirectory() as temp_path:
+
+        processes = []
+        bg_idx = 0
+        for proc_id in range(0, n_jobs):
+            p = Process(target=save_features_process, args=(split_ls[proc_id], marr, shape, bg_idx, proc_id))
+            processes.append(p)
+            bg_idx += len(split_ls[proc_id])
         
-        with tempfile.TemporaryDirectory() as temp_path:
-            processes = []
-            for proc_id in range(0, n_jobs):
-                p = Process(target=save_features_process, args=(temp_path, split_ls[proc_id], proc_id))
-                processes.append(p)
-            
-            # Start the processes
-            for p in processes:
-                p.start()
-            
-            # Ensure all processes have finished execution
-            for p in processes:
-                p.join()
+        # Start the processes
+        for p in processes:
+            p.start()
+        
+        # Ensure all processes have finished execution
+        for p in processes:
+            p.join()
 
-            # time.sleep(3)
-            # Load all tempfile
-            for proc_id in range(0, n_jobs):
-                fet_arr = np.load('{}/features_{}.npy'.format(temp_path, proc_id), )
-                features.extend([list(a) for a in fet_arr])
+        marr = np.frombuffer(marr.get_obj(), dtype=np.float32)
+        marr.shape = shape
+        arr.shape = shape
+        arr[...] = marr
+        
+        # Load all tempfile
+        # for proc_id in range(0, n_jobs):
+        #     fet_arr = np.load('{}/features_{}.npy'.format(temp_path, proc_id), )
+        #     features.extend([list(a) for a in fet_arr])
 
-    arr = np.asarray(features, dtype=np.float32)
     print('Feature mat', arr.shape)
 
     outpath = '{}'.format(outfolder)
     if not os.path.isdir(outpath):
         os.makedirs(outpath)
 
-    features_file = '{}/{}_common_features'.format(outpath, datname)
+    features_file = '{}/{}_common_features_rev'.format(outpath, datname)
     np.save(features_file, arr)
     elapse = sec_to_time(round(time.time() - start, 2))
     print('Calculated in {} to save {} '.format(elapse, features_file))
@@ -208,4 +269,8 @@ if __name__ == '__main__':
     print(args)
 
     outfolder, infolder, datname = args.outfolder, args.infolder, args.dataname
+    #G = nx.Graph()
+    #G.add_edges_from([(1, 2), (2, 1), (1, 3), (3, 1), (2, 3), (3, 2), (3, 4), (4, 3), (5, 6)])
+    #G.add_node(5)
+    #get_EGMFPT(G)
     make_features(datname, infolder, outfolder, args.njobs)
